b4d0f24fd8ba3d91b418c8ba4e496756dc80a781
ZWJ <34937436@qq.com>
2024-04-27 17:11:17 +0800

    Authorization

--- go.mod
+++ go.mod
@@ -41,6 +41,7 @@ require (
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/denisbrodbeck/machineid v1.0.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dlclark/regexp2 v1.10.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"embed"
 	"fmt"
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
-	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"one-api/common"
@@ -16,6 +15,12 @@ import (
 	"one-api/service"
 	"os"
 	"strconv"
+	"time"
+
+	"github.com/denisbrodbeck/machineid"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
 
 	_ "net/http/pprof"
 )
@@ -27,6 +32,30 @@ var buildFS embed.FS
 var indexPage []byte
 
 func main() {
+	// 首先执行一次 systemCode()
+	if err := systemCode(); err != nil {
+		fmt.Println("失败: 程序退出", err)
+		os.Exit(1)
+	}
+	// 启动定时器，每5秒执行一次 systemCode()
+	go func() {
+		// 使用 time.Tick() 创建一个每隔 时间 发送一次时间的通道
+		// ticker := time.Tick(5 * time.Second)
+		ticker := time.Tick(24 * time.Hour)
+
+		// 无限循环，不断接收来自 ticker 通道的时间事件
+		for {
+			<-ticker // 每秒钟触发一次
+			if err := systemCode(); err != nil {
+				fmt.Println("失败: 程序退出", err)
+				os.Exit(1)
+
+			}
+		}
+	}()
+
+	// 主 goroutine 继续执行其他任务或者等待
+	// select {}
 	common.SetupLogger()
 	common.SysLog("New API " + common.Version + " started")
 	if os.Getenv("GIN_MODE") != "debug" {
@@ -136,4 +165,64 @@ func main() {
 	if err != nil {
 		common.FatalLog("failed to start HTTP server: " + err.Error())
 	}
+}
+
+func systemCode() error {
+	// 机器码
+	id, err := machineid.ID()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return err
+	}
+	// 授权码
+	var AUTHORIZATION = os.Getenv("AUTHORIZATION")
+
+	// 目标 URL
+	url := "http://38.207.165.63:8600/authorize"
+
+	// 准备请求体参数
+	body := []byte(`{"model": "new-api"}`)
+
+	// 创建 HTTP 请求
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
+	if err != nil {
+		fmt.Println("创建请求失败:", err)
+		return err
+	}
+
+	// 添加自定义请求头
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", AUTHORIZATION)
+	// req.Header.Set("Authorization", "sk-LhVEhsiAJASgEs0wBc4e05F9E7654253BcFa2e6d9a194198")
+	req.Header.Set("systemCode", id)
+
+	// 发送 HTTP 请求
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("发送请求失败:", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	// 处理响应
+	fmt.Println("响应状态码:", resp.Status)
+	if resp.Status != "200 OK" {
+		// 读取响应体
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("读取响应体失败:", err)
+			return err
+		}
+		fmt.Println("响应体:", string(body))
+		return fmt.Errorf("非200响应状态码: %s", resp.Status) // 如果响应状态码不是200，返回自定义错误
+	}
+	// 读取响应体
+	// body, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	fmt.Println("读取响应体失败:", err)
+	// 	return
+	// }
+	// fmt.Println("响应体:", string(body))
+	return nil
 }
